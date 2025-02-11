5344825d0ea4afe05c2abef3a449df4dcb5070dc
Ij Israfil <israfilkhan494@gmail.com>
2025-02-11 15:53:05 +0000

    Revert "extras: Include prebuilt Accord music player"
    This reverts commit cd4245be4e1faeaceb204232ddec94a397f2ac32.

--- evolution.mk
+++ evolution.mk
@@ -5,13 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Accord
-TARGET_INCLUDE_ACCORD ?= true
-ifeq ($(TARGET_INCLUDE_ACCORD),true)
-PRODUCT_PACKAGES += \
-    Accord
-endif
-
 # Clocks (SystemUI)
 PRODUCT_PACKAGES += \
     SystemUIClocks-BigNum \
